5c114d3a85f1a2ffcc357446f3df75c98cefa6de
Kristian Klemon <klemon@unternehmertum.de>
2020-08-13 10:05:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+from msg_gan.datasets import FlatImageFolder, IgnoreLabelDataset
+from torchbox.losses.gan import StandardGANLoss, WassersteinGANLoss, RelativisticAverageHingeGANLoss
 from torchvision.datasets import CelebA
+from easydict import EasyDict as edict
 
 # G_channels_per_block = [512, 512, 512, 512, 256, 128, 64, 32, 16]
 # D_channels_per_block = [16, 32, 64, 128, 256, 512, 512, 512, 512]
@@ -8,13 +11,20 @@ G_channels_per_block = [128, 128, 128, 128, 64, 32, 16, 8, 8]
 D_channels_per_block = [8, 8, 16, 32, 64, 128, 128, 128, 128]
 
 model_configs = {
-    2 ** (i + 1): dict(G_channels=G_channels_per_block[:i],
-                       D_channels=D_channels_per_block[-i:])
+    2 ** (i + 1): edict(G_channels=G_channels_per_block[:i],
+                        D_channels=D_channels_per_block[-i:])
     for i in range(4, 11)
 }
 
 
-datasets = {
-    'celeba': lambda **kwargs: CelebA(**kwargs)
-}
+datasets = edict({
+    'celeba': lambda **kwargs: IgnoreLabelDataset(CelebA(**kwargs)),
+    'ffhq': lambda **kwargs: FlatImageFolder(**kwargs)
+})
 
+loss_functions = edict({
+    'gan_loss': StandardGANLoss,
+    'wgan': lambda **kwargs: WassersteinGANLoss(use_gp=False, **kwargs),
+    'wgan_gp': lambda **kwargs: WassersteinGANLoss(use_gp=True, **kwargs),
+    'ra_hinge': RelativisticAverageHingeGANLoss
+})
